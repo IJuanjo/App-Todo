fde486d942a68b06772fbb7741ef853a5631bdb4
juanjo <jorbegob@emeal.nttdata.com>
2025-08-04 10:44:00 -0500

    feat: add model for arquitecture mvvm task

---
+++ arquitectures/mvvm/src/model/database/taskDB.ts
@@ -0,0 +1,15 @@
+import { JSONFilePreset } from 'lowdb/node'
+import { Task } from '@models/task/dto/task.dto';
+
+
+interface DatabaseSchema {
+    tasks: Task[];
+}
+
+const defaultData: DatabaseSchema = {
+    tasks: []
+};
+
+const db = await JSONFilePreset<DatabaseSchema>('src/model/database/db.json', defaultData);
+
+export default db;
